5c989a71d402d52195ce8fe670eee35e9e45d7da
matteo-rossi90 <matteo.rossi1314@gmail.com>
2024-11-19 23:27:24 +0100

    feat:  modify structure

--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Pagina pubblica</h1>
+    <div class="container-fluid">
+        <h1 class="mx-4 mt-5">Pagina pubblica</h1>
+    </div>
 @endsection
--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,27 +1,30 @@
 @extends('layouts.app')
 @section('content')
 
-<div class="container">
-    <h2 class="fs-4 text-secondary my-4">
-        {{ __('Profile') }}
-    </h2>
-    <div class="card p-4 mb-4 bg-white shadow rounded-lg">
+<div class="container-fluid">
+    <div class="my-4 mx-4">
+        <h2 class="fs-4 text-secondary my-5">
+            {{ __('Profilo di')  }} {{ Auth::user()->name }}
+        </h2>
+        <div class="card p-4 mb-4 bg-white rounded-lg">
 
-        @include('profile.partials.update-profile-information-form')
+            @include('profile.partials.update-profile-information-form')
 
-    </div>
+        </div>
 
-    <div class="card p-4 mb-4 bg-white shadow rounded-lg">
-
-
-        @include('profile.partials.update-password-form')
-
-    </div>
-
-    <div class="card p-4 mb-4 bg-white shadow rounded-lg">
+        <div class="card p-4 mb-4 bg-white rounded-lg">
 
 
-        @include('profile.partials.delete-user-form')
+            @include('profile.partials.update-password-form')
+
+        </div>
+
+        <div class="card p-4 mb-4 bg-white rounded-lg">
+
+
+            @include('profile.partials.delete-user-form')
+
+        </div>
 
     </div>
 </div>
